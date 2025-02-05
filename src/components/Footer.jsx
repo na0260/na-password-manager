d4346ba8c018366270f7f615e925b5ac3636ac28
na0260 <nur.201002398@green.ac.bd>
2025-02-05 16:38:27 +0600

    complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,8 @@ const Footer = () => {
     return (
         <footer className="text-gray-600 body-font">
             <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-                <a href="#" className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+                <a href="#"
+                   className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
                     <span className="ml-3 text-xl text-gray-400">NA Password Manager</span>
                 </a>
                 <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">©
